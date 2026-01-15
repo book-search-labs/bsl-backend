b111cd31215b35382f49e1e217e682e11bcbab00
devOceanBlue <87791365+devOceanblue@users.noreply.github.com>
2026-01-15 22:20:05 +0900

    first commit

---
+++ src/main/java/com/devoceanblue/bslbackend/BslBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devoceanblue.bslbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BslBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BslBackendApplication.class, args);
+    }
+
+}
